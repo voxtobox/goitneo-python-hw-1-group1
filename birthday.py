38542abc71437c0e587f05951ccfd4c17097d548
Volodymyr Antoniuk <voxtobox@gmail.com>
2024-02-25 17:04:25 -0800

    refactor birthday

--- birthday.py
+++ birthday.py
@@ -3,24 +3,22 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 
 
-def main(users):
-    result = defaultdict(list)
-    today = datetime.today().date()
-    for user in users:
-        name = user['name']
-        birthday = user['birthday'].date()
-        birthday_this_year = birthday.replace(year=today.year)
-        if birthday_this_year < today:
-            birthday_this_year = birthday_this_year.replace(
-                year=today.year + 1)
-        delta_days = (birthday_this_year - today).days
-        if delta_days > 7:
-            continue
-        if (birthday_this_year.weekday() >= 5):
-            birthday_this_year = birthday_this_year + \
-                timedelta(days=7 - birthday_this_year.weekday())
+def get_norm_weekday_user_birthday(birthday):
+    weekday = birthday.weekday()
+    if (weekday >= 5):
+        birthday = birthday + timedelta(days=(7 - weekday))
+    return birthday
 
-        result[birthday_this_year.strftime("%A")].append(name)
+
+def get_user_birthday_this_year(user_birthday, today):
+    birthday = user_birthday.date()
+    birthday_this_year = birthday.replace(year=today.year)
+    if birthday_this_year < today:
+        birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+    return birthday_this_year
+
+
+def show(result):
     result_string = ''
     for day_name, names in result.items():
         names_string = ', '.join(names)
@@ -28,5 +26,15 @@ def main(users):
     print(result_string)
 
 
-if __name__ == "__main__":
-    main()
+def main(users):
+    result = defaultdict(list)
+    today = datetime.today().date()
+    for user in users:
+        name, birthday = user.values()
+        birthday_this_year = get_user_birthday_this_year(birthday, today)
+        birthday_this_year = get_norm_weekday_user_birthday(birthday_this_year)
+        delta_days = (birthday_this_year - today).days
+        if delta_days > 7:
+            continue
+        result[birthday_this_year.strftime("%A")].append(name)
+    show(result)
